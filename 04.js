94a93da4bc023858a8d3605ef7b52a0e35b9b7cb
= <=>
2021-10-25 16:06:36 +0600

    add file codewars example

---
+++ 04.js
@@ -0,0 +1,15 @@
+function check(a, x) {
+    let c = '';
+    for (let i=0; i< a.length; i++){
+      if (a[i] == x){
+        c = a[i]; 
+      }
+    }
+    if (c == x) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  console.log(check([66, 78], 66))
