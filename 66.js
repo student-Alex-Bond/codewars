0d2f9d0354feb37ffbb9c89847c9d52eef01f5de
student-Alex-Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2021-11-04 21:20:07 +0600

    add 66.js codewars

--- 66.js
+++ 66.js
@@ -1,4 +1,8 @@
-function greet(name){
-  //your code here
-  return `Hello, ${name} how are you doing today?`
+function isDivisible(n, x, y) {
+  
+  if (n%x ===0 && n%y===0){
+    return true
+  }else{
+    return false
+  }
 }
